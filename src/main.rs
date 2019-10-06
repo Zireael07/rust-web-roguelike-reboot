352d72647f3979edeefa7c42201bcf6fac7b61e3
Zireael07 <zi@mail.com>
2019-10-06 16:32:54 +0200

    new: HTML buttons able to pass enums back into Rust

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,32 @@ macro_rules! log {
     }
 }
 
+#[wasm_bindgen]
+#[repr(u8)] //single byte representation - enough for our needs since a byte can carry 256 things
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Command {
+    MoveLeft = 0,
+    MoveRight = 1,
+    MoveDown = 2,
+    MoveUp = 3,
+}
+
+#[wasm_bindgen]
+pub fn setCommand(com_id: i32){
+        log!("{}", com_id);
+        //need to match back to enum
+        // https://stackoverflow.com/questions/28028854/how-do-i-match-enum-values-with-an-integer
+        match com_id {
+            com_id if com_id == Command::MoveLeft as i32 => log!("{:?}", Command::MoveLeft),
+            com_id if com_id == Command::MoveRight as i32 => log!("{:?}", Command::MoveRight),
+            com_id if com_id == Command::MoveDown as i32 => log!("{:?}", Command::MoveDown),
+            com_id if com_id == Command::MoveUp as i32 => log!("{:?}", Command::MoveUp),
+            _ => log!("Unknown command"),
+}
+}
+
+
+
 pub fn main() {
  let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
