81c84965449b43f8e1d37f448b47ee3d80a5b05b
Zireael07 <zi@mail.com>
2019-11-13 17:23:03 +0100

    new: spawn remaining items from JSON

--- src/raws/rawmaster.rs
+++ src/raws/rawmaster.rs
@@ -76,6 +76,16 @@ pub fn spawn_named_item(raws: &RawMaster, new_entity : EntityBuilder, key : &str
             }
         }
 
+        if let Some(weapon) = &item_template.weapon {
+            eb = eb.with(Equippable{ slot: EquipmentSlot::Melee });
+            eb = eb.with(MeleePowerBonus{ power : weapon.power_bonus });
+        }
+        
+        if let Some(shield) = &item_template.shield {
+            eb = eb.with(Equippable{ slot: EquipmentSlot::Shield });
+            eb = eb.with(DefenseBonus{ defense: shield.defense_bonus });
+        }
+
         return Some(eb.build());
     }
     None
