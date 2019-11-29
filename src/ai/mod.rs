b2ad68707a91b548ee5e1a41abb30245ab4432ce
Zireael07 <zi@mail.com>
2019-11-29 17:29:37 +0100

    nuke a file which is no longer used

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,8 +1,5 @@
 mod bystander_ai_system;
-#[allow(non_snake_case)]
-mod NPC_ai_system;
 pub use bystander_ai_system::BystanderAI;
-pub use NPC_ai_system::NPCAI;
 mod initiative_system; 
 pub use initiative_system::InitiativeSystem;
 mod turn_status;
